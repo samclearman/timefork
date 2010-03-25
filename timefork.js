394c2e3bd5299c834d83586b3e5e61c0153f2f32
F1LT3R <f1lt3r@aqua.(none)>
2010-03-24 20:27:56 -0400

    Added mouse events.

--- timefork.js
+++ timefork.js
@@ -10,8 +10,20 @@
     idleTime: 1000,
     lastTime: new Date().getTime(),
     canvas  : undefined,
-    context : undefined
+    context : undefined,
+    style   : {
+                TWO_PI  : Math.PI*2,
+                diam    : 5,
+                spacing : 2.5,
+              }
   };
+
+  // Statenode constructor
+  var StateNode = function(props){
+    for(var i in props){ this[i] = props[i]; }
+    return this;
+  };
+
 
   // TimeFork Constructor
   var TimeFork = function( aName, elem, event, prop ){
@@ -28,9 +40,9 @@
     this.prop = prop;    
     
     // Set first leaf in statetree
-    this.stateTree[ 0 ].propState = this.elem[ this.prop ];
+    this.stateTree.propState = this.elem[ this.prop ];
     
-    this.pointInHistory = 0;
+    this.pointInHistory = this.stateTree;
     
     // Re-route Event Stream
     elem.addEventListener( event, function( event ){ origin.filter( event ); }, false );
@@ -38,44 +50,46 @@
     return this;
   };
   
-  TimeFork.prototype.stateTree = [{
+  TimeFork.prototype.stateTree = new StateNode({
       ancestor  : 0,
       branches  : [],
       eventTime : defs.lastTime,
       eventType : 'History began.',
       eventData : null,
       eventData : undefined
-  }];
-  
-/*  var StateNode = function(props){
-     for(var i in props){ this[i] = props[i]; }
-  };
-*/  
-  
+  });
+   
   TimeFork.prototype.filter = function( event ){
     clearTimeout( this.timeout );
-    var origin = this;  
+    var origin = this;
     this.timeout = setTimeout( function(){ origin.recordState( event ); }, this.idleTime );
   };  
   
   
   TimeFork.prototype.recordState = function( event ){
-    var now = new Date().getTime();
-    this.stateTree[ this.stateTree.length ] = {
+    var now = new Date().getTime();    
+    
+    this.pointInHistory = this.pointInHistory.branches[ 0 ] = new StateNode({
       ancestor  : this.lastTime,
       branches  : [],
       eventTime : now,
       eventType : this.event,
       eventData : event,
       propState : this.elem[ this.prop ],
-    };    
-    this.pointInHistory = this.stateTree.length -1;
+    });
+    
     console.log( this.pointInHistory );
     this.lastTime = now;
     this.render();
   };
 
   TimeFork.prototype.makeCanvas = function(){
+    var origin = this;
+    var mouseX=0, mouseY=0;
+    var mouseMoved = function(){
+      console.log(mouseX, mouseY); 
+    };
+    
     with( this ){
       canvas = document.createElement('canvas');
       canvas.setAttribute('width' , "600px");
@@ -85,7 +99,17 @@
       body.appendChild(canvas);
       context = canvas.getContext('2d');
     }
+    
+    this.canvas.addEventListener('mousemove', function( e ){
+      var scrollX = (window.scrollX !== null && typeof window.scrollX !== 'undefined') ? window.scrollX : window.pageXOffset;
+      var scrollY = (window.scrollY !== null && typeof window.scrollY !== 'undefined') ? window.scrollY : window.pageYOffset;     
+      mouseX = e.clientX - origin.canvas.offsetLeft + scrollX;
+      mouseY = e.clientY - origin.canvas.offsetTop + scrollY;
+      mouseMoved();
+    }, false);    
   };
+  
+  
     
   // Rendering time forks and state nodes
   TimeFork.prototype.render = function(){
@@ -95,34 +119,47 @@
         makeCanvas();
       }
       
-      context.fillStyle   = "#aaa";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      context.fillStyle   = "#0af";
-      context.strokeStyle = "#fff";
-      context.lineWidth   = 1;
-      
-      var diam = 5;
-      var x = diam,
-          y = canvas.height/2,
-          spacing = diam * 0.5;
-      
-      for(var i in stateTree){
-        node = stateTree[i];
-          
-        with( context ){
-          beginPath();
-            arc(x, y, diam, 0, Math.PI*2, true);
-          closePath();
-          fill();
-          stroke();
-        }
-                        
-        x+=diam*2+spacing;
-                         
+      with( context ){
+        fillStyle   = "#aaa";
+        fillRect(0, 0, canvas.width, canvas.height);
+        fillStyle   = "#0af";
+        strokeStyle = "#fff";
+        lineWidth   = 1;         
+                 
+        context.save();      
+          translate( style.diam, canvas.height/2 );
+          renderBranch( stateTree );        
+        restore();
       }
+
+    }
+  };  
+  
+
+  TimeFork.prototype.renderBranch = function( node ){     
+    var style = this.style;
+    for(var i in node.branches){
+      with( this.context ){
+        if( this.pointInHistory.eventTime === node.branches[i].eventTime ){
+          fillStyle = "#c00";
+          lineWidth = 2;
+        }else{
+          fillStyle = "#0af";
+          lineWidth = 1;
+        }
+        translate(style.diam*2+style.spacing,0);
+        beginPath();
+          arc(0, 0, style.diam, 0, style.TWO_PI, true);
+        closePath();
+        fill();
+        stroke();
+      }
+      this.renderBranch( node.branches[i] );    
     }
   };
   
+  
+  // Model instatiator
   this.timeFork = function( aName, elem, event, prop ){
     return new TimeFork( aName, elem, event, prop );
   };
